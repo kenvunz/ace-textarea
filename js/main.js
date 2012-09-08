7feaa5ee8c66c19849791387b73c0d9f89fbdeaf
Ken <kenvunz@gmail.com>
2012-09-08 14:41:55 +1200

    - clean up

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function($) {
-
 	$('.textarea').acetextarea();
 });
-
-// var editor = ace.edit("editor");
-// editor.setTheme("ace/theme/tomorrow_night");
-// editor.getSession().setMode("ace/mode/javascript");
